b771dae339e23d473d89de08fd94ac0f84fa59fb
Santi-004 <agustinsalassanti@gmail.com>
2025-06-22 00:33:54 -0300

    Creación de pages y agregando libreria de tailwinds.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import Button from "./components/Button";
+import './index.css';
+
 function App() {
   return(
     <p>Hola Mundo.</p>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        AzulClaro: '#c4d2e7',
+        Azul: '#688db9',
+        GrisOscuro: '#333333'
+      },
+    },
+  },
+  plugins: [],
+}
+
